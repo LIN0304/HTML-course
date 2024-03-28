707dbcfd6c77995557fb1b2dc9eb7ecd9862580a
Ray Lin <69388379+LIN0304@users.noreply.github.com>
2024-03-28 14:27:25 +0800

    Add files via upload

---
+++ 0321.php
@@ -0,0 +1,24 @@
+<form method="post" action="wp_ecom.php">
+
+心理學實驗：請任意輸入一個正整數，統計所有人的填答後，輸入的數字最小「且」唯一不重複者獲勝。<br>
+
+<label>你的名字</label><input name="name" type="text">
+
+<br>
+
+<label>數字</label><input name="number" type="number" min="1" step="1">
+
+<br>
+
+<label>是否支持老師幫獲勝者加分</label><input name="plus" type="radio" value="yes">加分
+<input name="plus" type="radio" value="no">不加分
+
+<br>
+
+<input type="submit">
+
+<br>
+
+
+
+</form>
