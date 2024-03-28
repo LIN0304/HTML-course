9b5c5921497c64170c5a0472766ce6fc3d1be341
Ray Lin <69388379+LIN0304@users.noreply.github.com>
2024-03-28 14:33:57 +0800

    Update 0321.php

--- 0321.php
+++ 0321.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Artistic Form</title>
 <style>
   body {
     font-family: 'Arial', sans-serif;
@@ -55,21 +49,46 @@
     background-color: #0056b3;
   }
 </style>
-</head>
-<body>
+
+
+
+
+
+
+
+
+
+
 
 <form method="post" action="wp_ecom.php">
-  <p>心理學實驗：請任意輸入一個正整數，統計所有人的填答後，輸入的數字最小「且」唯一不重複者獲勝。</p>
-  <label>你的名字</label><input name="name" type="text">
-  <br>
-  <label>數字</label><input name="number" type="number" min="1" step="1">
-  <br>
-  <label>是否支持老師幫獲勝者加分</label><input name="plus" type="radio" value="yes">加分
-  <input name="plus" type="radio" value="no">不加分
-  <br>
-  <input type="submit">
-  <br>
-</form>
 
-</body>
-</html>
+心理學實驗：請任意輸入一個正整數，統計所有人的填答後，輸入的數字最小「且」唯一不重複者獲勝。<br>
+
+<label>你的名字</label><input name="name" type="text">
+
+<br>
+
+<label>數字</label><input name="number" type="number" min="1" step="1">
+
+<br>
+
+<label>是否支持老師幫獲勝者加分</label><input name="plus" type="radio" value="yes">加分
+<input name="plus" type="radio" value="no">不加分
+
+<br>
+
+<label>對這份問卷評分</label><select name="score">
+<option>5</option>
+<option>4</option>
+<option>3</option>
+<option>2</option>
+<option>1</option>
+</select><br><br>
+
+<input type="submit">
+
+<br>
+
+
+
+</form>
